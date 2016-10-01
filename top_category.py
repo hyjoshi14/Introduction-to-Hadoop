dd6920f594b2941c8ed11c9669f3b336fa62fdad
hyjoshi14 <hyjoshi_14@hotmail.com>
2016-10-02 04:47:05 +0800

    Added filter in the mapper

--- top_category.py
+++ top_category.py
@@ -8,14 +8,14 @@ class TopCategory(MRJob):
             pass
         else:
             category, sales = data[3],float(data[4])
-            yield(category,sales)
+            if category == 'Toys' or category == 'Consumer Electronics':
+                yield(category,sales)
 
     def reducer(self,category,sales):
-        if category == 'Toys' or category == 'Consumer Electronics':
-            tot_sales = 0
-            for sale in sales:
-                tot_sales += sale
-            yield(category,tot_sales)
+        tot_sales = 0
+        for sale in sales:
+            tot_sales += sale
+        yield(category,tot_sales)
 
 if __name__ == '__main__':
     TopCategory.run()
